53f92e39a2b0841bd65d4214dc921401f98a59d3
FHARDER <frederikharder@gmail.com>
2016-11-22 17:20:44 +0100

    inits done

--- practical_2/exec_calls.sh
+++ practical_2/exec_calls.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "ex-1"
-python train_mlp.py --learning_rate 1e-3 --weight_init normal --weight_init_scale 1e-5 --weight_reg none --log_dir ./logs/cifar10/inits/norm5
-python train_mlp.py --learning_rate 1e-3 --weight_init normal --weight_init_scale 1e-4 --weight_reg none --log_dir ./logs/cifar10/inits/norm4
+#python train_mlp.py --learning_rate 1e-3 --weight_init normal --weight_init_scale 1e-5 --weight_reg none --log_dir ./logs/cifar10/inits/norm5
+#python train_mlp.py --learning_rate 1e-3 --weight_init normal --weight_init_scale 1e-4 --weight_reg none --log_dir ./logs/cifar10/inits/norm4
 python train_mlp.py --learning_rate 1e-3 --weight_init normal --weight_init_scale 1e-3 --weight_reg none --log_dir ./logs/cifar10/inits/norm3
-python train_mlp.py --learning_rate 1e-3 --weight_init normal --weight_init_scale 1e-2 --weight_reg none --log_dir ./logs/cifar10/inits/norm2
-python train_mlp.py --learning_rate 1e-3 --weight_init uniform --weight_init_scale 2e-5 --weight_reg none --log_dir ./logs/cifar10/inits/unif5
-python train_mlp.py --learning_rate 1e-3 --weight_init uniform --weight_init_scale 2e-4 --weight_reg none --log_dir ./logs/cifar10/inits/unif4
-python train_mlp.py --learning_rate 1e-3 --weight_init uniform --weight_init_scale 2e-3 --weight_reg none --log_dir ./logs/cifar10/inits/unif3
-python train_mlp.py --learning_rate 1e-3 --weight_init uniform --weight_init_scale 2e-2 --weight_reg none --log_dir ./logs/cifar10/inits/unif2
-python train_mlp.py --learning_rate 1e-3 --weight_init xavier --weight_reg none --log_dir ./logs/cifar10/inits/xavier
+#python train_mlp.py --learning_rate 1e-3 --weight_init normal --weight_init_scale 1e-2 --weight_reg none --log_dir ./logs/cifar10/inits/norm2
+#python train_mlp.py --learning_rate 1e-3 --weight_init uniform --weight_init_scale 2e-5 --weight_reg none --log_dir ./logs/cifar10/inits/unif5
+#python train_mlp.py --learning_rate 1e-3 --weight_init uniform --weight_init_scale 2e-4 --weight_reg none --log_dir ./logs/cifar10/inits/unif4
+#python train_mlp.py --learning_rate 1e-3 --weight_init uniform --weight_init_scale 2e-3 --weight_reg none --log_dir ./logs/cifar10/inits/unif3
+#python train_mlp.py --learning_rate 1e-3 --weight_init uniform --weight_init_scale 2e-2 --weight_reg none --log_dir ./logs/cifar10/inits/unif2
+#python train_mlp.py --learning_rate 1e-3 --weight_init xavier --weight_reg none --log_dir ./logs/cifar10/inits/xavier
 #echo "ex-2"
 #python train_mlp.py --activation tanh --learning_rate 1e-3 --weight_init normal --weight_init_scale 0.001 --weight_reg none --log_dir ./logs/cifar10/inacts/tanhnorm
 #python train_mlp.py --activation tanh --learning_rate 1e-3 --weight_init xavier --weight_reg none --log_dir ./logs/cifar10/inacts/tanhxavier
@@ -26,6 +26,6 @@ python train_mlp.py --learning_rate 1e-3 --weight_init xavier --weight_reg none 
 #python train_mlp.py --learning_rate 1e-3 --dnn_hidden_units 300,300 --activation relu --weight_init normal --weight_init_scale 1e-3 --optimizer rmsprop --weight_reg none --log_dir ./logs/cifar10/opts/rmsprop
 #python train_mlp.py --learning_rate 1e-3 --dnn_hidden_units 300,300 --activation relu --weight_init normal --weight_init_scale 1e-3 --optimizer sgd --weight_reg none --log_dir ./logs/cifar10/opts/sgd
 #echo "ex-5"
-vpython train_mlp.py --dnn_hidden_units 400 --learning_rate 1e-3 --weight_reg l2 \
+#python train_mlp.py --dnn_hidden_units 400 --learning_rate 1e-3 --weight_reg l2 \
 #--max_steps 3500 --batch_size 200 --dropout_rate 0. --activation elu --weight_init_scale 1e-3 \
 #--weight_init normal --optimizer adagrad --weight_reg_strength 0.2 --log_dir ./logs/cifar10/topmodel
