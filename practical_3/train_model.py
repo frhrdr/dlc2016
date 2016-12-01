bbaba168cf5b147c124f10614b2bead47161c151
Fr-d-rik <frederikharder@gmail.com>
2016-12-01 12:21:26 +0100

    convnet ready for testing

--- practical_3/train_model.py
+++ practical_3/train_model.py
@@ -83,8 +83,8 @@ def train():
     ########################
     cifar10 = cifar10_utils.get_cifar10(FLAGS.data_dir)
     cnn = ConvNet()
-    data_dims = list(cifar10.train.data.shape[1:])
-    num_classes = list(cifar10.train.target.shape[1])
+    data_dims = list(cifar10.train.images.shape[1:])
+    num_classes = list(cifar10.train.labels.shape[1])
     with tf.Graph().as_default():
         x_pl = tf.placeholder(dtype=tf.float32, shape=[FLAGS.batch_size] + data_dims)
         y_pl = tf.placeholder(dtype=tf.float32, shape=[FLAGS.batch_size, num_classes])
